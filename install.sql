2fcf3bf6f80c1c2ae78ee140ad8a6f16e49bf6f5
raoulmunet <raoulmunet@gmail.com>
2026-09-23 18:54:23 +0300

    Add install.sql

---
+++ install.sql
@@ -0,0 +1,9 @@
+prompt Installing Oracle Data Lineage & Impact Analysis Explorer...
+
+@sql/00_types.sql
+@sql/01_demo_objects.sql
+@sql/02_reporting_views.sql
+@plsql/pkg_lineage.pks
+@plsql/pkg_lineage.pkb
+
+prompt Installation complete.
